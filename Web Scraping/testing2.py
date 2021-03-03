b14418aa4bb2a38a7a0be69b3491391ebd60cb17
Frostbox31 <31751979+Frostbox31@users.noreply.github.com>
2021-03-04 01:27:44 +0800

    Create testing2.py

---
+++ Web Scraping/testing2.py
@@ -0,0 +1,16 @@
+#for match in soup.find_all('div',id=lambda value:value and value.startswith("t")):
+#for match in soup.find_all('h3',class_="_eYtD2XCVieq6emjKBH3m"):  # Title
+ #   headline = match.text
+  #  print(headline)
+
+#for match in soup.find_all('span',class_="FHCV02u6Cp2zYL0fhQPsO"):  # Number of Comment
+#    headline = match.text
+#    print(headline)
+
+#for match in soup.find_all('a', href=lambda value:value and value.startswith("/r/CryptoCurrency/comments/")): # Comment Link
+#    if match['href'] not in output:
+#        output.insert(0,match['href'])  
+
+#https://gateway.reddit.com/desktopapi/v1/subreddits/CryptoCurrency?rtj=only&redditWebClient=web2x&app=web2x-client-production&allow_over18=&include=prefsSubreddit&after=t3_lrqe9w&dist=9&layout=card&sort=hot&geo_filter=SG
+
+#print(soup.prettify())

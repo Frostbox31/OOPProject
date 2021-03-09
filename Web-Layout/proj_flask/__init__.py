190be04eb79b0411da59a6f035209e69224d0ec0
steees <kosoonteck@gmail.com>
2021-03-09 19:10:54 +0800

    update

--- Web-Layout/proj_flask/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from proj_flask import routes
---
+++ layout/proj_flask/db_connect.py
@@ -0,0 +1,8 @@
+import mysql.connector
+
+mysql.connector.connect(
+  host="127.0.0.1",
+  user="root",
+  password="root1",
+  database="DataProject"
+)

2e0a4e3b50bba04f8d754c2d5cfb03f63b62f3a5
Frostbox31 <31751979+Frostbox31@users.noreply.github.com>
2021-03-15 15:20:34 +0800

    Enter

---
+++ Web_scraping/Discord.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import date, datetime
+import mysql.connector
+import time
+from decimal import Decimal
+import sys
+import datetime 
+import discord
+
+headers = { 'authorization' : ''}

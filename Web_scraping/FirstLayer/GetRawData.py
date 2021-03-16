f35d045c8ab7f46e2ae0b690a7f6944b866996db
Frostbox31 <31751979+Frostbox31@users.noreply.github.com>
2021-03-16 12:32:05 +0800

    Update GetRawData.py

--- Web_scraping/FirstLayer/GetRawData.py
+++ Web_scraping/FirstLayer/GetRawData.py
@@ -1,10 +1,10 @@
-from SecondLayer.Google import *
+from SecondLayer.Google import getGoogleNew
 from SecondLayer.CoinGecko import *
 from GetResult import getcoinsname
 import time
 from decimal import Decimal
 from time import sleep
-
+import numpy as np
 
 CalculateTime = Decimal(time.perf_counter())
 
@@ -14,11 +14,13 @@ CalculateTime = Decimal(time.perf_counter())
 def _GenerateGoogleNews():
 
     coinsname = getcoinsname()
+    coinsname.insert(0,"cryptocurrency")
 
     for x in range (len(coinsname)):
-        sleep(2)
-        print(str(coinsname[x]).replace('(','').replace(')','').replace(',','').replace("'",''))
-        #getGoogleNew(str(coinsname[x]).replace('(','').replace(')','').replace(',',''))
+        delays = [6, 7, 10, 12]
+        delay = np.random.choice(delays)
+        sleep(delay) 
+        getGoogleNew(str(coinsname[x]).replace('(','').replace(')','').replace(',','').replace("'",'').replace("-"," "))
 
 _GenerateGoogleNews()
 CalculateTime = Decimal(time.perf_counter()) - CalculateTime

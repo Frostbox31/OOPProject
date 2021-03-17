c261a325e9d56988d0f4e7a8e524c976d41fbdd2
Frostbox31 <31751979+Frostbox31@users.noreply.github.com>
2021-03-18 00:41:48 +0800

    Update GetRawData.py

--- Web_scraping/FirstLayer/GetRawData.py
+++ Web_scraping/FirstLayer/GetRawData.py
@@ -1,36 +1,36 @@
-from SecondLayer.Google import getGoogleNew
-from SecondLayer.Yahoo import getyahoofinancecomment
-from SecondLayer.CoinGecko import *
-from GetResult import getcoinsname, getcoinsshortform
+from SecondLayer.Google import getGoogleNews
+from SecondLayer.Yahoo import getYahooFinanceComment
+from SecondLayer.CoinGecko import _getHistoricalDataForAllCoin,_getDailyVolumeForAllCoins
+from GetResult import getCoinsName, getCoinsShortForm
 import time
 from decimal import Decimal
 from time import sleep
 import numpy as np
 
-CalculateTime = Decimal(time.perf_counter())
+CalculateTime = Decimal(time.perf_counter()) #Start the Timer
 
-#getdailyvolumeforallcoins()
-#gethistoricaldataforallcoin()
-
-def _GenerateGoogleNews():
-    coinsname = getcoinsname()
+def _generateGoogleNews():
+    coinsname = getCoinsName()
     coinsname.insert(0,"cryptocurrency")
 
     for x in range (len(coinsname)):
         delays = [6, 7, 10, 12]
         delay = np.random.choice(delays)
         sleep(delay) 
-        getGoogleNew(str(coinsname[x]).replace('(','').replace(')','').replace(',','').replace("'",'').replace("-"," "))
+        getGoogleNews(str(coinsname[x]).replace('(','').replace(')','').replace(',','').replace("'",'').replace("-"," "))
 
-def _GenerateYahooFinanceTopComment():
-    coinsname = getcoinsname()
-    coinshortform = getcoinsshortform()
+def _generateYahooFinanceTopComment():
+    coinsname = getCoinsName()
+    coinshortform = getCoinsShortForm()
 
     for x in range (len(coinsname)):   
-        getyahoofinancecomment(str(coinsname[x]).replace('(','').replace(')','').replace(',','').replace("'",'').replace("-"," "),str(coinshortform[x]).replace('(','').replace(')','').replace(',','').replace("'",'').replace("DOT",'DOT1'))
+        getYahooFinanceComment(str(coinsname[x]).replace('(','').replace(')','').replace(',','').replace("'",'').replace("-"," "),str(coinshortform[x]).replace('(','').replace(')','').replace(',','').replace("'",'').replace("DOT",'DOT1'))
 
-#_GenerateGoogleNews()
-_GenerateYahooFinanceTopComment()
-CalculateTime = Decimal(time.perf_counter()) - CalculateTime
-print(str(CalculateTime) + " Seconds")
+#_getDailyVolumeForAllCoins() #Get Cryptocurrency Trading Volume Each Day
+_getHistoricalDataForAllCoin() #Get Cryptocurrency MarketCap,Volume, Opening Price and Closing since it listed
+#_generateGoogleNews() #Get Cryptocurrency and Top 10 Coins Google News
+#_generateYahooFinanceTopComment() # Get Cryptocurrency people comment from Yahoo Finance
+
+CalculateTime = Decimal(time.perf_counter()) - CalculateTime # Calculate the difference between the time
+print(str(CalculateTime) + " Seconds") #Display the time taken for the program to run
 

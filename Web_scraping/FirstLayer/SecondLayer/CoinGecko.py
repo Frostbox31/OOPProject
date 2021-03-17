575d98b11201f913d722ffa71218e61625fb31a3
Frostbox31 <31751979+Frostbox31@users.noreply.github.com>
2021-03-18 00:41:54 +0800

    Update CoinGecko.py

--- Web_scraping/FirstLayer/SecondLayer/CoinGecko.py
+++ Web_scraping/FirstLayer/SecondLayer/CoinGecko.py
@@ -1,9 +1,7 @@
-from SecondLayer.Utilities import commitsqlcommand
+from SecondLayer.Utilities import _commitSQLCommand
 import requests
 from bs4 import BeautifulSoup
 from datetime import date
-
-
 
 class Data:
       def __init__(coin,name,date,marketcap, volume,open,close):
@@ -14,7 +12,7 @@ class Data:
           coin.open = open
           coin.close = close
 
-def sublink():
+def __sublink(): #Get Cryptocurrency Link
     
   output = []
   enddate = date.today().strftime("%Y-%m-%d")
@@ -28,110 +26,98 @@ def sublink():
   for match in soup.find_all('a', href=lambda value:value and value.startswith("/en/coins") and not value.endswith(tuple(endwith))): # Comment Link
             if "https://www.coingecko.com" + match['href'] + "/historical_data/usd?end_date="+ enddate + "&start_date=" + startdate not in output:
                 output.insert(0,"https://www.coingecko.com" + match['href'] + "/historical_data/usd?end_date="+ enddate + "&start_date=" + startdate)
-
   return output
 pass
 
-def gethistoricaldataforallcoin():
+def _getHistoricalDataForAllCoin(): # Get Cryptocurrency MarketCap,Volume, Opening Price and Closing since it listed
 
-  link = sublink()
-  
+  link = __sublink()
   dataset =  []
-  temp = []
-  temp2 = []
+  date = []
+  coindata = []
 
-  for position in range(len(link)-90): #change this to get the number of coins,Currently 10 coins
+  for position in range(len(link)-90): #change this to get the number of coins, For EG: 99 = 1 Coin, 98 = 2 Coin
     
       request = requests.get(link[99-position],headers={'User-agent': 'Super Bot Power Level Over 9000'})
 
-      temp3 = link[99-position].split("/")[5]
+      coinname = link[99-position].split("/")[5]
 
       soup = BeautifulSoup(request.content, 'html.parser')
 
       match = soup.find('table')
       
-      for match in soup.find_all(attrs={"scope": 'row'}): # Comment Link
-          temp.insert(len(temp),match.text)
+      for match in soup.find_all(attrs={"scope": 'row'}):
+          date.insert(len(date),match.text)
 
-      for match in soup.find_all("td",class_='text-center'): # Comment Link
-          temp2.insert(len(temp2),match.get_text(strip=True))
+      for match in soup.find_all("td",class_='text-center'):
+          coindata.insert(len(coindata),match.get_text(strip=True))
     
       inc =0;
-      for x in range(len(temp)):
-          dataset.insert(len(dataset),Data(temp3,temp[x],temp2[inc],temp2[1+inc],temp2[2+inc],temp2[3+inc]))   
+      for x in range(len(date)):
+          dataset.insert(len(dataset),Data(coinname,date[x],coindata[inc],coindata[1+inc],coindata[2+inc],coindata[3+inc]))   
           inc += 4
-      temp.clear()
-      temp2.clear()
-  commitsqlcommand('DELETE FROM CoinGeckoData','')
+    
+  _commitSQLCommand('DELETE FROM CoinGeckoData','')
   for x in range(len(dataset)):
-    commitsqlcommand('INSERT INTO CoinGeckoData (Name, Date, MarketCap, Volume, Open, Close) VALUES (%s, %s, %s, %s, %s, %s)',(str(dataset[x].name),str(dataset[x].date),str(dataset[x].marketcap),str(dataset[x].volume).replace('$',''),str(dataset[x].open),str(dataset[x].close)))
+    _commitSQLCommand('INSERT INTO CoinGeckoData VALUES (%s, %s, %s, %s, %s, %s)',(dataset[x].name,dataset[x].date,dataset[x].marketcap,str(dataset[x].volume).replace('$',''),dataset[x].open,dataset[x].close))
 pass
 
-
-def getdailyvolumeforallcoins():
+def _getDailyVolumeForAllCoins(): # Get Cryptocurrency Trading Volume Each Day
   
-    class Volume:
-      def __init__(coin,name,volume):
-          coin.name = name
-          coin.volume = volume
-
-    temp = []
-    num = []
-    temp4 = []
+    coinname = []
+    coinshortform = []
+    volume = []
     pagenumber = 1
     check = pagenumber
 
     while pagenumber == check:
-      temp2 = []
-      temp3 = []
+      coinvolumeprice = []
+      coinprice = []
       request = requests.get('https://www.coingecko.com/en/coins/all/show_more_coins?page=' + str(pagenumber),headers={'User-agent': 'Super Bot Power Level Over 9000'})
 
       soup = BeautifulSoup(request.content, 'html.parser')
 
       for match in soup.find_all(class_="d-none d-lg-block font-bold"):
-          temp.insert(len(temp),match.get_text(strip=True))
+          coinname.insert(len(coinname),match.get_text(strip=True))
       
       for match in soup.find_all(class_="d-none d-lg-inline font-normal text-3xs mt-1"):
-          temp4.insert(len(temp4),str(match.get_text(strip=True)).encode('cp1252', errors='ignore'))
+          coinshortform.insert(len(coinshortform),str(match.get_text(strip=True)).encode('cp1252', errors='ignore'))
 
       for match in soup.find_all(class_="td-total_volume lit text-right px-0 pl-2"):
           if match.get_text(strip=True) == "$0.00000000" or match.get_text(strip=True) == "?" :
-              temp2.insert(len(temp2),"0")
+              coinvolumeprice.insert(len(coinvolumeprice),"0")
           else:
-              temp2.insert(len(temp2),str(match.get_text(strip=True)).replace(',','').replace('$',''))
+              coinvolumeprice.insert(len(coinvolumeprice),str(match.get_text(strip=True)).replace(',','').replace('$',''))
     
-      count = 0
       for match in soup.find_all(attrs={"data-target": 'price.price'}):
             if match.has_attr('data-coin-id'):
                 if match.get_text(strip=True) == "$0.00000000" or match.get_text(strip=True) == "?" :
-                    temp3.insert(len(temp3),"0")
+                    coinprice.insert(len(coinprice),"0")
                 else:
-                    temp3.insert(len(temp3),str(match.get_text(strip=True)).replace(',','').replace('$',''))
+                    coinprice.insert(len(coinprice),str(match.get_text(strip=True)).replace(',','').replace('$',''))
           
-      for x in range (len(temp2)):
-          if(temp2[x] != "0" and temp3[x] != "0"):
-             num.insert(len(num),float(temp2[x])//float(temp3[x]))
+      #Divide the total Cryptocurrency with its Price(According to the each Crypto)
+      for x in range (len(coinvolumeprice)):
+          if(coinvolumeprice[x] != "0" and coinprice[x] != "0"):
+             volume.insert(len(volume),float(coinvolumeprice[x])//float(coinprice[x]))
           else:
-             num.insert(len(num),0)
+             volume.insert(len(volume),0)
 
-      if len(temp) % 300 == 0:
+      if len(coinname) % 300 == 0:
           pagenumber += 1
           check += 1
       else:
           check -= 1
     pass
     
-    print(len(temp4))
-    print(len(temp))
-
-    commitsqlcommand('DELETE FROM coinvolume','')
+    _commitSQLCommand('DELETE FROM coinvolume','')
     totalvolume = 0
 
-    for x in range(len(temp)):
-        commitsqlcommand('INSERT INTO coinvolume VALUES (%s,%s, %s, %s)',(x+1,temp[x],(float(num[x])),temp4[x]))
-        totalvolume += num[x]
+    for x in range(len(coinname)):
+        _commitSQLCommand('INSERT INTO coinvolume VALUES (%s,%s, %s, %s)',(x+1,coinname[x],volume[x],coinshortform[x]))
+        totalvolume += volume[x]
     
-    commitsqlcommand('INSERT INTO coinvolume VALUES (%s,%s, %s,%s)',(len(temp)+1,"Total Volume",float(totalvolume),'C'))
+    _commitSQLCommand('INSERT INTO coinvolume VALUES (%s,%s, %s,%s)',(len(coinname)+1,"Total Volume",totalvolume,'C'))
 pass
 
 

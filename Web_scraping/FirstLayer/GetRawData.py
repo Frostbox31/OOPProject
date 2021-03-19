c8b4e4bac8f1c14b2c79cf29ca5cb5e438ea1126
Frostbox31 <31751979+Frostbox31@users.noreply.github.com>
2021-03-19 11:54:11 +0800

    Update GetRawData.py
    Changes Version 2:2:3
    
    Input IF - Else statement to prevent error

--- Web_scraping/FirstLayer/GetRawData.py
+++ Web_scraping/FirstLayer/GetRawData.py
@@ -11,29 +11,46 @@ import numpy as np
 CalculateTime = Decimal(time.perf_counter()) #Start the Timer
 
 def _generateGoogleYahooReddit(param,param2):
-    coinsname = getCoinsName()
 
-    if 'Reddit' == param:
-        _getRedditCoinOccur(coinsname,getCoinsShortForm(),param2)
-    
-    elif 'Yahoo' == param:
-        coinshortform = getCoinsShortForm()
-        for x in range (len(coinsname)):   
-            _getYahooFinanceComment(coinsname[x],str(coinshortform[x]).replace("DOT",'DOT1'),param2)
+ if param == "Reddit" or param == "Yahoo" or param == "Google":
 
-    elif 'Google' == param:
-        coinsname.insert(0,"cryptocurrency")
-        for x in range (len(coinsname)):
-            delays = [6, 7, 10, 12]
-            delay = np.random.choice(delays)
-            sleep(delay) 
-            _getGoogleNews(coinsname[x])
+    if isinstance(param2,int) == True or param == "Google":
+
+                coinsname = getCoinsName()
+
+                if 'Reddit' == param and param2 > 0:
+                    _getRedditCoinOccur(coinsname,getCoinsShortForm(),param2)
+                
+                elif 'Yahoo' == param and param2 % 30 == 0 and param2 > 0:
+                    coinshortform = getCoinsShortForm()
+                    for x in range (len(coinsname)):   
+                        _getYahooFinanceComment(coinsname[x],str(coinshortform[x]).replace("DOT",'DOT1'),param2)
+
+                elif 'Google' == param:
+                    coinsname.insert(0,"cryptocurrency")
+                    for x in range (len(coinsname)):
+                        delays = [6, 7, 10, 12]
+                        delay = np.random.choice(delays)
+                        sleep(delay) 
+                        _getGoogleNews(coinsname[x])
+
+                elif 'Yahoo' == param and param2 % 30 != 0:
+                    print("Please input the number in multiple of 30 for yahoo function")
+                
+                elif param2 <= 0:
+                    print("Please input the parameter 2 starting from 1")
+                
+    else:
+        print("Please input interger for parameter 2")
+ else:
+    print("Please Specific the function")
+
 
 
 #_getDailyVolumeForAllCoins()           # Get Cryptocurrency Trading Volume Each Day
-_getHistoricalDataForAllCoin(1)         # Get Cryptocurrency MarketCap,Volume, Opening Price and Closing since it listed
-#_generateGoogleYahooReddit('Yahoo',150)    # Get Cryptocurrency people comment from Yahoo Finance in multiple of 30
-#_generateGoogleYahooReddit('Google','')   # Get Cryptocurrency and Top 10 Coins Google News
+#_getHistoricalDataForAllCoin(10)         # Get Cryptocurrency MarketCap,Volume, Opening Price and Closing since it listed
+#_generateGoogleYahooReddit('Yahoo',1020)    # Get Cryptocurrency people comment from Yahoo Finance in multiple of 30
+#_generateGoogleYahooReddit('Google','')   # Get Cryptocurrency and Top 10 Coins Google News according to coingecko table
 #_generateGoogleYahooReddit('Reddit',1)    # Get Cryptocurrency Coins Ocurrence in Reddit  EG 1 = 6 or 7 posts
 CalculateTime = Decimal(time.perf_counter()) - CalculateTime # Calculate the difference between the time
 print(str(CalculateTime) + " Seconds") #Display the time taken for the program to run

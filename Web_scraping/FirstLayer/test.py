ed0ff99b90ebb77cbe743c633034d3d0da00c9c3
Frostbox31 <31751979+Frostbox31@users.noreply.github.com>
2021-03-21 14:41:29 +0800

    Update test.py
    Split the test case into three section

--- Web_scraping/FirstLayer/test.py
+++ Web_scraping/FirstLayer/test.py
@@ -2,33 +2,34 @@ import unittest
 from GetRawData import _getDailyVolumeForAllCoins, _getHistoricalDataForAllCoin,_generateGoogleYahooReddit
 from GetResult import getTotalVolumeForTheDay,getCoinsName,getCoinsShortForm
 
-class TestFunction(unittest.TestCase):
-    def test_function(self):
+class TestCase(unittest.TestCase):
+    def test_GetResult(self):
 
+       self.assertTrue(getTotalVolumeForTheDay())                       #True because this function return value
+       self.assertTrue(getCoinsName())                                  #True because this function return value
+       self.assertTrue(getCoinsShortForm())                             #True because this function return value
+
+    def test_CoinGecko(self):
        self.assertFalse(_getDailyVolumeForAllCoins())                   #False because this function return value
-
        self.assertFalse(_getHistoricalDataForAllCoin(1))                #False because this function return value          
        self.assertFalse(_getHistoricalDataForAllCoin(10))               #False because this function return value
        self.assertFalse(_getHistoricalDataForAllCoin(0))                #False because this function return value   
        self.assertFalse(_getHistoricalDataForAllCoin(100))              #False because this function return value
        self.assertFalse(_getHistoricalDataForAllCoin('ABC'))            #False because this function return value
 
+    def test_GoogleYahooReddit(self):
        self.assertFalse(_generateGoogleYahooReddit('Yahoo',150))        #False because this function doesn't return any value
        self.assertFalse(_generateGoogleYahooReddit('Yahoo','ABC'))      #False because this function doesn't return any value
        self.assertFalse(_generateGoogleYahooReddit('Yahoo',1000))       #False because this function doesn't return any value
        self.assertFalse(_generateGoogleYahooReddit('Yahoo',1020))       #False because this function doesn't return any value
        self.assertFalse(_generateGoogleYahooReddit('Yahoo',0))          #False because this function doesn't return any value
-
-       #self.assertFalse(_generateGoogleYahooReddit('Google',''))
-
+       self.assertFalse(_generateGoogleYahooReddit('Google',''))
        self.assertFalse(_generateGoogleYahooReddit('Reddit',1))         #False because this function doesn't return any value
        self.assertFalse(_generateGoogleYahooReddit('Reddit','ABC'))     #False because this function doesn't return any value
        self.assertFalse(_generateGoogleYahooReddit('Reddit',-3))        #False because this function doesn't return any value
-       self.assertFalse(_generateGoogleYahooReddit('',''))                #False because this function doesn't return any value
- 
-       self.assertTrue(getTotalVolumeForTheDay())                       #True because this function return value
-       self.assertTrue(getCoinsName())                                  #True because this function return value
-       self.assertTrue(getCoinsShortForm())                             #True because this function return value
-
+       self.assertFalse(_generateGoogleYahooReddit('',''))                #False because this function doesn't return any value                      
+    
 if __name__ == '__main__':
  unittest.main()
+
+
